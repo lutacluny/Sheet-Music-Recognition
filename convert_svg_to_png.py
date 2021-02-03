38238f6194a0171b68da1c49f5f1d0122f6906e3
kocahan <onurkocahan@gmail.com>
2021-02-03 03:10:27 +0100

    pre_process double -> int mismatch bug correction + convert_svg_to_png -> size for 224 224

--- convert_svg_to_png.py
+++ convert_svg_to_png.py
@@ -10,42 +10,41 @@ from PIL import  Image
 import shutil
 import os
 
-category = 'tests'
+category = 'notes'
 
 def main():
     if os.path.isdir("png_{}".format(category)):
         shutil.rmtree("png_{}".format(category))
-       
-    shutil.copytree("svg_{}".format(category), "png_{}".format(category))   
-    
-    
+
+    shutil.copytree("svg_{}".format(category), "png_{}".format(category))
+
+
     for dirName, subdirList, fileList in os.walk("png_{}".format(category)):
         for fName in fileList:
             svgLocation = "{}/{}".format(dirName, fName)
-        
+
             fNameNew = fName[:-4] + ".png"
-            imgLocation = "{}/{}".format(dirName, fNameNew)      
-        
-            svg2png(url=svgLocation, write_to=imgLocation)
+            imgLocation = "{}/{}".format(dirName, fNameNew)
+
+            svg2png(url=svgLocation, write_to=imgLocation,output_width=224, output_height=224)
 
             note = Image.open(imgLocation)
             make_background_white(note)
-            converted = note.convert('L') 
+            converted = note.convert('L')
             converted.save(imgLocation)
-                
+
             os.remove(svgLocation)
 
 def make_background_white(note):
     note_with_white_background = []
-    
+
     for data in note.getdata():
         if data == (0,0,0,0):
             note_with_white_background.append((255,255,255,255))
         else:
             note_with_white_background.append((0,0,0,255))
 
-    note.putdata(note_with_white_background)    
+    note.putdata(note_with_white_background)
 
-if __name__=="__main__": 
-    main() 
-
+if __name__=="__main__":
+    main()
