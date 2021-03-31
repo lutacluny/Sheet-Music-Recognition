94097e8c3368e7bb557f297c330812db6b184ef6
kocahan <onurkocahan@gmail.com>
2021-03-31 12:11:07 +0300

    Matlab flexibility for testing model + parameters

--- music_ocr_cnn.m
+++ music_ocr_cnn.m
@@ -1,17 +1,24 @@
 clear; close all; clc
 %location of resized training images .png
-location = 'C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test2\png_notes_2\';
+
+location = 'C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\generate_database\png_objects_new_2\';
 imds = imageDatastore(location, 'IncludeSubfolders', true, 'LabeLSource', 'foldernames');
 %location of the resized test preprocessed images 
-test_location_matlab = 'C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test2\pre_processed_matlab\line_2.png\line_2\';
+%C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\object_detection\separated_notes
+test_location_matlab = 'C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\object_detection\separated_notes\Der Mond ist aufgegangen\line_0';
 imds_test = imageDatastore(test_location_matlab);
+imds_test.Files
 perm = randperm(2250,20);
 % for i = 1:20
 %     subplot(4,5,i);
 %     imshow(imds.Files{perm(i)});
 % end
 
-
+test_location_matlab_last = 'C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\object_detection\separated_notes\';
+%
+%
+%
+%
 labelCount = countEachLabel(imds);
 
 img = readimage(imds,1);
@@ -20,7 +27,7 @@ img_size = size(img);
 %numTrainFiles = 2250;
 %split training validation  test 0.6 0.2 0.2
 %real test data is different -> those are for the generated ones
-[imdsTrain,imdsValidation,imdsTest] = splitEachLabel(imds,0.6,0.2,'randomize');
+[imdsTrain,imdsValidation,imdsTest] = splitEachLabel(imds,0.6,0.2');
 
 
 % layers = [
@@ -48,7 +55,9 @@ img_size = size(img);
 % 
 
 
-net=googlenet;
+net=resnet50;
+%net=googlenet;
+
 if isa(net,'SeriesNetwork') 
   lgraph = layerGraph(net.Layers); 
 else
@@ -80,26 +89,104 @@ lgraph = createLgraphUsingConnections(layers,connections);
 
 options = trainingOptions('sgdm', ...
     'InitialLearnRate',0.005, ...
-    'MaxEpochs',5, ...
+    'MaxEpochs',3, ...
     'Shuffle','every-epoch', ...
-    'ValidationData',imdsValidation, ...
+    'ValidationData',imdsValidation,...
     'ValidationFrequency',30, ...
     'Verbose',false, ...
     'Plots','training-progress',...,
+    'MiniBatchSize',32, ...
 'ExecutionEnvironment','gpu');
 
+    
 
-net = trainNetwork(imdsTrain,lgraph,options);
-
-
-YPred = classify(net,imdsValidation);
+net = trainNetwork(imds,lgraph,options);
+% i=1;
+% imshow(imds_test_location_matlab_last.Files{i});
+% classify(net,imread(imds_test_location_matlab_last.Files{i}))
+ YPred = classify(net,imdsValidation);
 YValidation = imdsValidation.Labels;
 accuracy = sum(YPred == YValidation)/numel(YValidation);
 
 
 pred_test = classify(net,imdsTest);
 test_Acc=sum(pred_test == imdsTest.Labels)/size(pred_test,1);
+mapObj = containers.Map({'Der Mond ist aufgegangen','Fuchs du hast die Gans gestohlen', 'Schlaf Kindlein Schlaf','Schlaf Main Kinderl Schlaf','Simple Melody','Speed the Plough','Spottlied auf den 1574 aus Krakau entflohenen Polenkoenig','Wilhelmus von Nassouwe'}, {1, 2,3,4,5,6,7,8});
 
-realTest_Res= classify(net,imds_test);
+TesterVariable = [];
+%('name',"",'real',[],"predicted",[]);
+dirtest='C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\object_detection\separated_notes_2'
+
+imds_test_location_matlab_last_simple = imageDatastore('C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\object_detection\separated_notes_2\Simple Melody\line_0\', 'IncludeSubfolders',true,'FileExtensions',{'.png'})
+imds_test_location_matlab_last = imageDatastore(dirtest, 'IncludeSubfolders',true,'FileExtensions',{'.png'})
+listing = dir(dirtest);
+EvalArr={};
+diroutput='C:\Users\ONUR\Desktop\matlab\Matlab Project\smc_new\test_last\Sheet-Music-Recognition\process_output\output_label_files\'
+for j=3:size(listing,1)
+    songName=listing(j).name;
+    listingFileLine = dir(dirtest+"\"+songName);
+    target=diroutput+convertCharsToStrings(songName);
+    fid = fopen(target,'wt');
+   
+for li=3:size(listingFileLine,1)
+    lineStr=listingFileLine(li).name;
+    listingFileNotes = dir(dirtest+"\"+songName+"\"+lineStr);
+    imds_test_line = imageDatastore(dirtest+"\"+songName+"\"+lineStr,'FileExtensions',{'.png'});
+    pred=strings([size(imds_test_line.Files,1),1]);
+    lineRes= classify(net,imds_test_line);
+    Rescells=cellstr(lineRes);
+    for i=1:size(imds_test_line.Files,1) 
+    TestClass=split(imds_test_line.Files{i},"\separated_notes_2\");
+    TestCass=split(TestClass(2),"\line_");
+    lineNo=TestCass(2);
+    lineNo=split(lineNo,"\note");
+    lineNo=lineNo(1);
+    TestClass=TestCass(1);
+    splitted=split(imds_test_line.Files{i},"note_");
+    splitted=split(splitted(2),".");
+     index=str2num(splitted{1})+1;
+    pred(index)= Rescells{i};
+        
+    end
+%      imds_test_line.Files
+%      lineRes
+%      Rescells
+%      pred
+%     
+    for jl=1:size(pred,1)
+        
+         fprintf(fid,replace(replace(pred(jl),"_top",""),"_bottom","")+" ");
+    end
+             fprintf(fid,"\n");
+
+end
+    fclose(fid);
+
+  % EvalArr{mapObj(listing(i).name)}=struct("name",listing(i).name);
+end
+listingGroundTruth = dir(dirtest);
+
+lolo=13;
+realTest_Res(lolo,:)
+imds_test_location_matlab_last_simple.Files(lolo)
+realTest_Resacc(lolo)
+[realTest_Resacc,realTest_Res]= classify(net,imds_test_location_matlab_last_simple);
 
 
+pred=strings([size(imds_test_location_matlab_last.Files,1),1]);
+for i=1:size(imds_test_location_matlab_last.Files,1) 
+    TestClass=split(imds_test_location_matlab_last.Files{i},"\separated_notes_2\");
+    TestCass=split(TestClass(2),"\line_");
+    lineNo=TestCass(2);
+    lineNo=split(lineNo,"\note");
+    lineNo=lineNo(1);
+    
+    TestClass=TestCass(1);
+    splitted=split(imds_test_location_matlab_last.Files{i},"note_");
+    splitted=split(splitted(2),".")
+  index=str2num(splitted{1})+1;
+ cells=cellstr(realTest_Resacc);
+ pred(index)= cells{index};
+     disp("Song:" +TestClass+"   Line No:"+lineNo + "     Index="+index );
+EvalArr{mapObj(TestClass)}
+end
